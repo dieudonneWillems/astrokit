666067b54b941e6fc97e50d735414805ae80295e
lapsedPacifist <don_willems@me.com>
2014-03-22 13:18:10 +0100

    Started on Coordinate functions
    Created struct for points in spherical coordinates (3D).

---
+++ AstroKit/AKCoordinateFunctions.h
@@ -0,0 +1,18 @@
+//
+//  AKCoordinateFunctions.h
+//  AstroKit
+//
+//  Created by Don Willems on 22/03/14.
+//  Copyright (c) 2014 lapsedpacifist. All rights reserved.
+//
+
+#ifndef __ASTROKIT__COORDINATE_FUNCTIONS_
+#define __ASTROKIT__COORDINATE_FUNCTIONS_
+
+typedef struct __sphericalposition {
+    double longitude;
+    double latitude;
+    double distance;
+} AKPoint;
+
+#endif

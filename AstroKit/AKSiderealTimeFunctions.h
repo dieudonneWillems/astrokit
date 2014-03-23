d9e26846dcda338f80008274ba3f958d1dd375db
lapsedPacifist <don_willems@me.com>
2014-03-23 10:45:50 +0100

    Added Sidereal Time functions
    Started on Sidereal Time.

---
+++ AstroKit/AKSiderealTimeFunctions.h
@@ -0,0 +1,19 @@
+//
+//  AKSiderealTime.h
+//  AstroKit
+//
+//  Created by Don Willems on 23/03/14.
+//  Copyright (c) 2014 lapsedpacifist. All rights reserved.
+//
+
+
+#import "AKTimeFunctions.h"
+
+
+
+#ifndef __ASTROKIT__TIME_FUNCTIONS_
+#define __ASTROKIT__TIME_FUNCTIONS_
+
+double AKMeanSiderealTimeAtGreenwich(AKJulianCenturies T);
+
+#endif
---
+++ AstroKit/AKSiderealTimeFunctions.m
@@ -0,0 +1,15 @@
+//
+//  AKSiderealTime.m
+//  AstroKit
+//
+//  Created by Don Willems on 23/03/14.
+//  Copyright (c) 2014 lapsedpacifist. All rights reserved.
+//
+
+#import "AKSiderealTimeFunctions.h"
+
+
+double AKMeanSiderealTimeAtGreenwich(AKJulianCenturies T)
+{
+    return 0;
+}
